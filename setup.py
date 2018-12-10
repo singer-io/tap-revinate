6b6148bb5cb97feb6383d281cb9ea212b88b8519
Kyle Allan <kyle@stitchdata.com>
2018-12-10 20:02:09 +0000

    move to singer

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-revinate',
-      version='0.1.0',
+      version='1.0.0',
       description='Singer.io tap for extracting data from the Revinate Porter API',
       author='Bytecode IO',
       url='http://www.singer.io',
