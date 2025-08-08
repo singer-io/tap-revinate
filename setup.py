15440a73d3cf2b5acd422fdd8e0c448f74d4a645
Dylan Sprayberry <28106103+dsprayberry@users.noreply.github.com>
2025-08-08 14:41:35 -0400

    SAC-28323: CircleCI Upgrade, Swap to UV (#2)
    * Move from old circle to new circle config
    
    * install deps in circle steps
    
    * pylint disables
    
    * moare pylint disables
    
    * bump dep versions
    
    ---------
    
    Co-authored-by: Ben Allred <ben.allred@qlik.com>

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@
 from setuptools import setup
 
 setup(name='tap-revinate',
-      version='1.0.0',
+      version='1.0.1',
       description='Singer.io tap for extracting data from the Revinate Porter API',
       author='Bytecode IO',
       url='http://www.singer.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_revinate'],
       install_requires=[
-          'singer-python==5.2.0',
-          'backoff==1.3.2',
-          'requests==2.20.0',
+          'singer-python==5.13.2',
+          'backoff==1.10.0',
+          'requests==2.32.4',
           'python-dateutil==2.7.3',
           'pybase64==0.4.0',
           'pendulum==2.0.3'
